313d9f338e9e600267866ba5c190a0461a4d220a
junwon <junwonseo95@gmail.com>
2021-07-30 17:06:50 +0900

    F1 score test

--- TEST.py
+++ TEST.py
@@ -1,18 +1,18 @@
 import re
 import Levenshtein
 
+
 # PER '|' , LOC '$' , ORG '{'
 
 def get_cer(tar, pred):
-
     for x in '$|{]':
-        tar.replace(x,'')
-        pred.replace(x,'')
+        tar.replace(x, '')
+        pred.replace(x, '')
 
     return Levenshtein.distance(tar, pred), len(tar)
 
-def get_ne_cer(tar, pred):
 
+def get_ne_cer(tar, pred):
     distance = 0
     length = 0
 
@@ -21,36 +21,70 @@ def get_ne_cer(tar, pred):
         p = p.replace(' ', '')
 
     if len(tar) == len(pred):
-        for t,p in zip(tar, pred):
-            distance += Levenshtein.distance(t,p)
+        for t, p in zip(tar, pred):
+            distance += Levenshtein.distance(t, p)
             length += len(t)
 
     elif len(tar) < len(pred):
         for t in tar:
-            distance += min(map(lambda x: Levenshtein.distance(t,x), pred))
+            distance += min(map(lambda x: Levenshtein.distance(t, x), pred))
             length += len(t)
 
     elif len(tar) > len(pred):
         for p in pred:
-            candidates =list(map(lambda x: Levenshtein.distance(p,x), tar))
+            candidates = list(map(lambda x: Levenshtein.distance(p, x), tar))
             optimal = min(candidates)
             distance += optimal
-            length += len(candidates.index(optimal))
+            length += len(tar[candidates.index(optimal)])
 
     return distance, length
 
 
+def get_f1_precision(tar, pred, tags):
+
+    target_pos = [i for i, v in enumerate(tar) if v in tags]
+    prediction_pos = [i for i, v in enumerate(pred) if v in tags]
+
+    count = 0
+
+    for i in prediction_pos:
+        if min([abs(i-x) for x in target_pos]) <=2:
+            print()
+            #TODO
 
 
 
-#---------- read data -----------
+def printCER():
+    print('\ntotal CER: {:.3f}'.format(total_distance / total_length))
+    print('named-entity CER: {:.3f}'.format(ne_distance / ne_length))
+
+    print('\nPER tags: {:d}'.format(per_cnt))
+    if per_cnt > 0:
+        print('PER tag CER: {:.3f}'.format(per_distance / per_length))
+
+    print('\nLOC tags: {:d}'.format(loc_cnt))
+    if loc_cnt > 0:
+        print('LOC tag CER: {:.3f}'.format(loc_distance / loc_length))
+
+    print('\nORG tags: {:d}'.format(org_cnt))
+    if org_cnt > 0:
+        print('ORG tag CER: {:.3f}'.format(org_distance / org_length))
+
+
+def printF1():
+    print()
+
+
+# ---------- read data -----------
 f = open("TEST/true_transcripts.txt", 'rt', encoding="UTF8")
 f2 = open("OUTPUTS/NER-OUT/final_output.txt", 'rt', encoding="UTF8")
 
 targets = f.readlines()
 predictions = f2.readlines()
 
-#---------- statistics ----------
+# ---------- statistics ----------
+
+# CER
 total_distance = 0
 total_length = 0
 
@@ -70,12 +104,15 @@ per_cnt = 0
 loc_cnt = 0
 org_cnt = 0
 
-for target, prediction in zip(targets,predictions):
+# F1
+precision = 0
+recall = 0
 
+for target, prediction in zip(targets, predictions):
+    # -------------- check CER ---------------
     PER = re.findall('\|.*?]', target)
     LOC = re.findall('\$.*?]', target)
     ORG = re.findall('\{.*?]', target)
-    PER()
 
     P_PER = re.findall('\|.*?]', prediction)
     P_LOC = re.findall('\$.*?]', prediction)
@@ -85,7 +122,7 @@ for target, prediction in zip(targets,predictions):
     total_distance += dist
     total_length += length
 
-    dist, length = get_ne_cer(PER+LOC+ORG, P_PER+P_LOC+P_ORG)
+    dist, length = get_ne_cer(PER + LOC + ORG, P_PER + P_LOC + P_ORG)
     ne_distance += dist
     ne_length += length
 
@@ -101,27 +138,22 @@ for target, prediction in zip(targets,predictions):
     org_distance += dist
     org_length += length
 
-    per_cnt += len(PER)
-    loc_cnt += len(LOC)
-    org_cnt += len(ORG)
+    per_cnt += len(P_PER)
+    loc_cnt += len(P_LOC)
+    org_cnt += len(P_ORG)
+
+# --------------- check F1 ---------------
+
 
 
 
 print('------------------TEST RESULTS-------------------')
-print('validation set size: ' + str(len(targets)))
-print('Character Error Rates')
-print('total CER: ' + str(total_distance/total_length))
-print('named-entity CER: ' + str(ne_distance/ne_length))
+print('validation set size: {:d}'.format(len(targets)))
 
-print('PER tags: ' + str(per_cnt))
-if per_cnt:
-    print('PER tag CER: ' + str(per_distance/per_length))
+print('\n---Character Error Rates---')
 
-print('LOC tags: ' + str(loc_cnt))
-if loc_cnt > 0:
-    print(loc_cnt)
-    print('LOC tag CER: ' + str(loc_distance/loc_length))
+printCER()
 
-print('ORG tags: ' + str(org_cnt))
-if org_cnt:
-    print('ORG tag CER: ' + str(org_distance/org_length))
+print('\n---F1 Score Measures---')
+
+printF1()
