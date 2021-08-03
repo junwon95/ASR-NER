d3619ba04dc8ce6dad20356c27131a6ea9e4ecc6
junwon <junwonseo95@gmail.com>
2021-08-03 09:26:12 +0900

    added masking module

--- audioMask.py
+++ audioMask.py
@@ -5,7 +5,6 @@ audio_path = r'INPUT/test.pcm'
 
 signal = np.memmap(audio_path, dtype='h', mode='r').astype('float32')/ 32767
 
-
 # noise
 duration = 30000
 
@@ -15,14 +14,18 @@ esm = np.arange(duration)
 noise = 1 * np.sin(2 * np.pi * esm * freq_hz / sps)
 
 # configure
-mask_point = 20368
-front_padding = np.zeros(20368)
+mask_start = 20368
+front_padding = np.zeros(mask_start)
 
-end_point = 50368
-back_padding = np.zeros(signal.shape[0] - end_point)
+mask_end = 50368
+back_padding = np.zeros(signal.shape[0] - mask_end)
 
 mask = np.append(front_padding, noise)
 mask = np.append(mask, back_padding)
+
+# silence original
+signal[mask_start:mask_start+duration] = 0
+
 
 masked_signal = signal + mask
 soundfile.write('OUTPUTS/filename.wav', masked_signal, sps)
